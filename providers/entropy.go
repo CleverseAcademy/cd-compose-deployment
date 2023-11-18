b6a6fd3ead814b496c7af5dd37b8e4b20f404cea
intaniger <intaniger@gmail.com>
2023-11-18 12:35:44 +0700

    fix(providers): :lock: added mutex to the entropy methods

--- providers/entropy.go
+++ providers/entropy.go
@@ -4,12 +4,14 @@ import (
 	"crypto/sha512"
 	"encoding/base64"
 	"hash"
+	"sync"
 
 	"github.com/pkg/errors"
 )
 
 type Entropy struct {
 	h hash.Hash
+	sync.RWMutex
 }
 
 type IEntropy interface {
@@ -26,11 +28,17 @@ func NewEntropyGenerator(initialEntropy []byte) *Entropy {
 }
 
 func (e *Entropy) Update(data []byte) error {
+	e.Lock()
+	defer e.Unlock()
+
 	_, err := e.h.Write(data)
 	return errors.Wrap(err, "Entropy.Update")
 }
 
 func (e *Entropy) Base64Get() string {
+	e.RLock()
+	defer e.RUnlock()
+
 	hashedBytes := e.h.Sum(nil)
 
 	return base64.StdEncoding.EncodeToString(hashedBytes)
